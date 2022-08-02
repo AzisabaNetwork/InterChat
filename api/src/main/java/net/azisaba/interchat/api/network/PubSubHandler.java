b7cd29983ba4eb6af9aac32b8405011ba3cb4813
acrylic-style <me@acrylicstyle.xyz>
2022-08-02 18:17:11 +0900

    Remove unused import

--- api/src/main/java/net/azisaba/interchat/api/network/PubSubHandler.java
+++ api/src/main/java/net/azisaba/interchat/api/network/PubSubHandler.java
@@ -24,7 +24,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 public class PubSubHandler implements Closeable {

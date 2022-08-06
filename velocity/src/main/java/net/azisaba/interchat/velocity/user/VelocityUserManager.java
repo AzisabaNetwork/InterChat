dd71c3a818b760716c86b293f6011acaf04a07bb
acrylic-style <me@acrylicstyle.xyz>
2022-08-07 02:24:43 +0900

    Make it compile

--- velocity/src/main/java/net/azisaba/interchat/velocity/user/VelocityUserManager.java
+++ velocity/src/main/java/net/azisaba/interchat/velocity/user/VelocityUserManager.java
@@ -3,10 +3,12 @@ package net.azisaba.interchat.velocity.user;
 import net.azisaba.interchat.api.InterChatProvider;
 import net.azisaba.interchat.api.user.User;
 import net.azisaba.interchat.api.user.UserManager;
+import net.azisaba.interchat.api.util.ResultSetUtil;
 import net.azisaba.interchat.velocity.database.DatabaseManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -25,6 +27,23 @@ public class VelocityUserManager implements UserManager {
                     } else {
                         future.completeExceptionally(new NoSuchElementException("No user found with id " + uuid));
                     }
+                });
+            } catch (Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+        return future;
+    }
+
+    @Override
+    public @NotNull CompletableFuture<List<User>> fetchUserByUsername(@NotNull String username) {
+        CompletableFuture<List<User>> future = new CompletableFuture<>();
+        InterChatProvider.get().getAsyncExecutor().execute(() -> {
+            try {
+                DatabaseManager.get().runPrepareStatement("SELECT * FROM `players` WHERE `name` = ?", stmt ->{
+                    stmt.setString(1, username);
+                    ResultSet rs = stmt.executeQuery();
+                    future.complete(ResultSetUtil.toList(rs, User::createByResultSet));
                 });
             } catch (Throwable t) {
                 future.completeExceptionally(t);

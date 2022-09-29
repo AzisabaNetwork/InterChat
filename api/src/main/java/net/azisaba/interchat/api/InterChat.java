c65ed116ad1cb7e6838a7c90308e3bb421915ea6
acrylic-style <me@acrylicstyle.xyz>
2022-09-29 10:27:06 +0900

    Remove @NonExtendable from InterChat

--- api/src/main/java/net/azisaba/interchat/api/InterChat.java
+++ api/src/main/java/net/azisaba/interchat/api/InterChat.java
@@ -2,12 +2,10 @@ package net.azisaba.interchat.api;
 
 import net.azisaba.interchat.api.guild.GuildManager;
 import net.azisaba.interchat.api.user.UserManager;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.Executor;
 
-@ApiStatus.NonExtendable
 public interface InterChat {
     @NotNull
     Logger getLogger();

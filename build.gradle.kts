5b94d9fa68b933589313ffed2b33f7665a6ed86b
acrylic-style <me@acrylicstyle.xyz>
2022-09-30 22:03:48 +0900

    :bookmark: 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     }
 
     group = "net.azisaba.interchat"
-    version = "2.0.0-SNAPSHOT"
+    version = "2.0.0"
 
     java {
         toolchain.languageVersion.set(JavaLanguageVersion.of(8))

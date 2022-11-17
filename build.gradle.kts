230c1edd86d0fbbe2cb9875f2c306c4370601175
acrylic-style <me@acrylicstyle.xyz>
2022-11-17 21:57:09 +0900

    :bookmark: 2.1.2 - Fix replace order

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     }
 
     group = "net.azisaba.interchat"
-    version = "2.1.1"
+    version = "2.1.2"
 
     java {
         toolchain.languageVersion.set(JavaLanguageVersion.of(8))

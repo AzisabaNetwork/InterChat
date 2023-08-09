c3619eae1178a6b4290ae200283fb15110db33b3
acrylic-style <me@acrylicstyle.xyz>
2023-08-10 00:36:30 +0900

    fix prefix/suffix calculation

--- api/src/main/java/net/azisaba/interchat/api/data/LuckPermsUserDataProvider.java
+++ api/src/main/java/net/azisaba/interchat/api/data/LuckPermsUserDataProvider.java
@@ -26,8 +26,20 @@ public class LuckPermsUserDataProvider implements UserDataProvider {
     public static <T extends ChatMetaNode<?, ?>> @NotNull Map<@NotNull String, @NotNull String> getServerToMetaValueMap(@NotNull UUID uuid, @NotNull Class<T> clazz) {
         List<ChatMetaNodeData> list = new ArrayList<>();
         User user = LuckPermsProvider.get().getUserManager().loadUser(uuid).join();
+        Map<String, Set<String>> groupServerMap = new HashMap<>();
         for (Node node : user.data().toCollection()) {
-            if (clazz.isInstance(node)) {
+            if (node.getKey().startsWith("group.")) {
+                String groupName = node.getKey().substring("group.".length());
+                Set<String> servers = node.getContexts().getValues("server");
+                if (servers.isEmpty()) {
+                    groupServerMap.put(groupName, Collections.emptySet());
+                    continue;
+                }
+                try {
+                    groupServerMap.computeIfAbsent(groupName, k -> new HashSet<>()).addAll(servers);
+                } catch (UnsupportedOperationException ignored) {}
+            } else if (clazz.isInstance(node)) {
+                // node assigned to user
                 Set<String> servers = node.getContexts().getValues("server");
                 int priority = ((ChatMetaNode<?, ?>) node).getPriority();
                 String metaValue = ((ChatMetaNode<?, ?>) node).getMetaValue();
@@ -39,7 +51,15 @@ public class LuckPermsUserDataProvider implements UserDataProvider {
                     int groupWeight = group.getWeight().orElse(0);
                     for (Node node : group.data().toCollection()) {
                         if (clazz.isInstance(node)) {
-                            Set<String> servers = node.getContexts().getValues("server");
+                            // node assigned to group
+                            Set<String> servers = new HashSet<>(node.getContexts().getValues("server"));
+                            Set<String> groupServer = groupServerMap.getOrDefault(group.getName(), Collections.emptySet());
+                            if (!servers.isEmpty() && !groupServer.isEmpty()) {
+                                servers.removeIf(s -> !groupServer.contains(s));
+                                if (servers.isEmpty()) return; // example: user inherits the group with server=srv1 and group inherits meta with server=srv2
+                            } else if (servers.isEmpty() && !groupServer.isEmpty()) {
+                                servers.addAll(groupServer);
+                            }
                             int priority = ((ChatMetaNode<?, ?>) node).getPriority();
                             String metaValue = ((ChatMetaNode<?, ?>) node).getMetaValue();
                             list.add(new ChatMetaNodeData(servers, priority, metaValue, groupWeight));
